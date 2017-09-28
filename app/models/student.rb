0579efb511d1a473089bc22c5fd483ef5ae23255
ian-ramos <ramos.ianc@gmail.com>
2017-09-28 10:51:48 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -13,4 +13,15 @@
 class Student < ActiveRecord::Base
   has_many :classroom_students
   has_many :classrooms, through: :classroom_students
+
+
+  def self.search(search_term)
+    @students = Student.all
+    if search_term.empty?
+      @students
+    else
+      @students.select { |student| student.name.match(search_term.capitalize)}
+    end
+  end
+
 end
